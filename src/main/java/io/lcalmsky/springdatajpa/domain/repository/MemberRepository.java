267150a9c80caaee18455b924cd93c8ed2a55127
Jamie <lcalmsky@gmail.com>
2021-06-26 03:45:36 +0900

    Set JPA and DB and check the operation #1

---
+++ src/main/java/io/lcalmsky/springdatajpa/domain/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package io.lcalmsky.springdatajpa.domain.repository;
+
+import io.lcalmsky.springdatajpa.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
