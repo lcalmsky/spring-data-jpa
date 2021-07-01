a218bb8324b40b5d5d222004f9b1b02109df8ca8
Jaime <lcalmsky@gmail.com>
2021-07-01 20:38:09 +0900

    Develop bulk update functions #5

--- src/main/java/io/lcalmsky/springdatajpa/domain/repository/MemberRepository.java
+++ src/main/java/io/lcalmsky/springdatajpa/domain/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -38,4 +39,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(value = "select m from Member m left join Team t",
             countQuery = "select count(m) from Member m")
     Page<Member> findMembers(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int increaseAgeOfAllMembersOver(@Param("age") int age);
+
+    Member findByUsername(String username);
 }
