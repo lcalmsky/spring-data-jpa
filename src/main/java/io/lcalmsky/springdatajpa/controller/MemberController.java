4ab92833aac723213c8dc23e5a6de569d8def330
Jaime <lcalmsky@gmail.com>
2021-07-09 10:29:42 +0900

    Apply domain converter #10

---
+++ src/main/java/io/lcalmsky/springdatajpa/controller/MemberController.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.springdatajpa.controller;
+
+import io.lcalmsky.springdatajpa.domain.entity.Member;
+import io.lcalmsky.springdatajpa.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("홍길동", 20));
+    }
+}
